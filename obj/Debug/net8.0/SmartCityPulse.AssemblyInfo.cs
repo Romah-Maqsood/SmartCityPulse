e8ef15586b4b5785b1df3667e27a09ecdb9081e2
Taqdees Arshad <taqdees795@gmail.com>
2026-05-17 02:39:17 +0500

    Landing page updated

--- obj/Debug/net8.0/SmartCityPulse.AssemblyInfo.cs
+++ obj/Debug/net8.0/SmartCityPulse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCityPulse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aede71dee0df4717716ed02613545ce98dbfc108")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc1b27321f27ea1a185cca0ccd9c1bae8b3c021f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCityPulse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCityPulse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
